95de3e1e09d88c839b3daa0437e7e8aa05faf88e
zachdelano <z.delano@outlook.com>
2020-11-01 14:46:12 -0700

    mimic yew futures example

---
+++ frontend/README.md
@@ -0,0 +1,6 @@
+# Frontend
+
+## Yew Examples
+
+* [Callbacks](https://github.com/yewstack/yew/blob/master/examples/js_callback)
+* [Futures](https://github.com/yewstack/yew/tree/master/examples/futures)
--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -1,26 +1,97 @@
+use std::{
+    error::Error,
+    fmt::{self, Debug, Display, Formatter},
+};
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use wasm_bindgen_futures::JsFuture;
 use yew::prelude::*;
-// use yew::format::Json;
+use yewtil::future::LinkFuture;
+use yew::{html, Component, ComponentLink, Html, ShouldRender};
+use yew::services::ConsoleService;
+use sentinel::Camera;
+use web_sys::{Request, RequestInit, RequestMode, Response};
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct FetchError {
+    err: JsValue,
+}
+
+impl Display for FetchError {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        Debug::fmt(&self.err, f)
+    }
+}
+
+impl Error for FetchError {}
+
+impl From<JsValue> for FetchError {
+    fn from(value: JsValue) -> Self {
+        Self { err: value }
+    }
+}
+
+/// The possible states a fetch request can be in.
+pub enum FetchState<T> {
+    NotFetching,
+    Fetching,
+    Success(T),
+    Failed(FetchError),
+}
+
+async fn fetch_resource(url: &'static str) -> Result<String, FetchError> {
+    let mut opts = RequestInit::new();
+    opts.method("GET");
+    // opts.mode(RequestMode::Cors);
+
+    let request = Request::new_with_str_and_init(url, &opts)?;
+
+    let window = yew::utils::window();
+    let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
+    let resp: Response = resp_value.dyn_into().unwrap();
+
+    let text = JsFuture::from(resp.text()?).await?;
+    Ok(text.as_string().unwrap())
+}
 
 struct Model {
     link: ComponentLink<Self>,
     value: i64,
+    cameras: FetchState<String>
 }
 
 enum Msg {
-    AddOne,
+    SetFetchStateCameras(FetchState<String>),
+    GetCameras,
 }
 
 impl Component for Model {
     type Message = Msg;
     type Properties = ();
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
-        Self { link, value: 0 }
+        Self {
+            link,
+            value: 0,
+            cameras: FetchState::Fetching
+        }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::AddOne => self.value += 1,
+            Msg::SetFetchStateCameras(state) => {
+                self.cameras = state;
+                ()
+            },
+            Msg::GetCameras => {
+                self.link.send_future(async {
+                    match fetch_resource("http://localhost:8000/cameras").await {
+                        Ok(res) => {
+                            Msg::SetFetchStateCameras(FetchState::Success(res))
+                        },
+                        Err(err) => Msg::SetFetchStateCameras(FetchState::Failed(err)),
+                    }
+                })
+            }
         }
         true
     }
@@ -33,11 +104,20 @@ impl Component for Model {
     }
 
     fn view(&self) -> Html {
-        html! {
-            <div>
-                <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
-                <p>{ self.value }</p>
-            </div>
+        match &self.cameras {
+            FetchState::NotFetching => html! { "Not fetching" },
+            FetchState::Fetching => html! {
+                <>
+                    <button onclick=self.link.callback(|_| Msg::GetCameras)>
+                        { "Get Cameras" }
+                    </button>
+                </>
+            },
+            FetchState::Success(data) => {
+                println!("{:?}",data);
+                html! { "Success" }
+            },
+            FetchState::Failed(err) => html! { "Failed" },
         }
     }
 }
